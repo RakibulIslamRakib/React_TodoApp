69360e5689a455bbfa8ce36a4fc192f61ecfd2d2
RakibulIslamRakib <56424436+RakibulIslamRakib@users.noreply.github.com>
2021-08-11 00:11:09 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Posts from './components/Posts'
+
+function App() {
+  return (
+    <>
+      <h1 className='p-4 text-center bg-blue-50'>Todo-App</h1>
+    <Posts/>
+    </>
+  );
+}
+
+export default App;
