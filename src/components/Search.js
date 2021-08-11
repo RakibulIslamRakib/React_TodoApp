d36fde5cc06380f153c06eeca39a591d014e5cf3
RakibulIslamRakib <56424436+RakibulIslamRakib@users.noreply.github.com>
2021-08-11 03:20:53 -0700

    Update Search.js

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,19 @@
 import {useCallback,useState,useEffect} from 'react';
+import './GlobalVariables';
 
 function GetPostsData(props) {
 
   const handleChange = (value)=>{
-    let newPosts = (props.showPosts).filter(function(post){
-      return (post.title.toLowerCase().indexOf(value.toLowerCase())) >-1 })
-    props.updatePosts(newPosts);
+    fetch(global.postsUrl)
+    .then((response) => response.json())
+    .then((posts) => {
+      let newPosts = posts.filter(function(post){
+        return (post.title.toLowerCase().indexOf(value.toLowerCase())) >-1 });
+      props.updatePosts(newPosts)
+    })
+    .catch(err=>{
+      alert(`${err.name} occurs! Check your internet connection`)
+    })
   }
 
   const debounce = (func)=>{
@@ -19,13 +27,13 @@ function GetPostsData(props) {
       },500);
     }
   }
-//
+
   const optimizedFn = useCallback(debounce(handleChange),[]);
 
   return (
     <input  type='text' onChange={(e)=>optimizedFn(e.target.value)} 
-         placeholder='Search Todo' 
-           className="px-2 py-1 border-2 h-12  rounded-xl shadow-sm m-1 w-5/12"/>
+      placeholder='Search Todo' 
+      className="px-2 py-1 border-2 h-12  rounded-xl shadow-sm m-1 w-5/12"/>
   );
 }
 
